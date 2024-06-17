340cb522f3a24e7ac664c45a8bbf475af1a0488d
Linus Sommer <95619282+linus-md@users.noreply.github.com>
2024-06-17 13:45:40 +0200

    Add general equations (#2)

--- readme.md
+++ readme.md
@@ -10,3 +10,12 @@ from k_pendulum import pendulum
 >>> pendulum(1)
 ([x1^2 + y1^2 - 1], [u1, v1, x1*l1, y1*l1 - 1, 0])
 ```
+
+or a more general equation 
+
+```
+
+from k_pendulum import general_pendulum
+>>> general_pendulum(1)
+([x1^2 + y1^2 - 1], [u1, v1, x1*l1, y1*l1 - 1, dl1])
+```
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pytest
 from sage.all import PolynomialRing, QQ
-from main import pendulum
+from main import pendulum, general_pendulum
 
 def test_k_1():
     names = 'x1, y1, u1, v1, l1'
@@ -14,6 +14,19 @@ def test_k_1():
         R('0')
     ]  
     assert pendulum(1) == (qs, ps)
+
+def test_general_k_1():
+    names = 'x1, y1, u1, v1, l1, dl1'
+    R = PolynomialRing(QQ, names=names, order='invlex')
+    qs = [R('x1^2 + y1^2 - 1')]
+    ps = [
+        R('u1'),
+        R('v1'),
+        R('l1*x1'),
+        R('l1*y1 - 1'),
+        R('dl1')
+    ]  
+    assert general_pendulum(1) == (qs, ps)
 
 
 def test_k_2():
@@ -33,6 +46,26 @@ def test_k_2():
         R('0')
     ]
     assert pendulum(2) == (qs, ps)
+
+
+def test_general_k_2():
+    names = 'x1, y1, u1, v1, x2, y2, u2, v2, l1, l2, dl1, dl2'
+    R = PolynomialRing(QQ, names=names, order='invlex')
+    qs = [R('x1^2 + y1^2 - 1'), R('(x2-x1)^2 + (y2-y1)^2 - 1')]
+    ps = [
+        R('u1'),
+        R('v1'), 
+        R('- l1*x1 - l2*(x1-x2)'), 
+        R('- l1*y1 - l2*(y1-y2) - 1'),
+        R('u2'),
+        R('v2'), 
+        R('- l2*(x2-x1)'),
+        R('- l2*(y2-y1) - 1'),
+        R('dl1'),
+        R('dl2')
+    ]
+    assert general_pendulum(2) == (qs, ps)
+
 
 def test_k_3():
     names = 'x1, y1, u1, v1, x2, y2, u2, v2, x3, y3, u3, v3, l1, l2, l3'
@@ -60,3 +93,31 @@ def test_k_3():
         R('0')
     ]
     assert pendulum(3) == (qs, ps)
+
+def test_general_k_3():
+    names = 'x1, y1, u1, v1, x2, y2, u2, v2, x3, y3, u3, v3,' \
+            + 'l1, l2, l3, dl1, dl2, dl3'
+    R = PolynomialRing(QQ, names=names, order='invlex')
+    qs = [
+        R('x1^2 + y1^2 - 1'), 
+        R('(x2-x1)^2 + (y2-y1)^2 - 1'), 
+        R('(x3-x2)^2 + (y3-y2)^2 - 1')
+    ]
+    ps = [
+        R('u1'), 
+        R('v1'), 
+        R('- l1*x1 - l2*(x1 - x2)'), 
+        R('- l1*y1 - l2*(y1 - y2) - 1'), 
+        R('u2'), 
+        R('v2'), 
+        R('- l2*(x2 - x1) - l3*(x2 - x3)'), 
+        R('- l2*(y2 - y1) - l3*(y2 - y3) - 1'), 
+        R('u3'), 
+        R('v3'), 
+        R('- l3*(x3 - x2)'), 
+        R('- l3*(y3 - y2) - 1'),
+        R('dl1'),
+        R('dl2'),
+        R('dl3')
+    ]
+    assert general_pendulum(3) == (qs, ps)
